9604a0c061050848ab44e266d0a43270a0f1ef2d
sasoripathos <sasori.pan.jerry@gmail.com>
2019-08-25 06:58:02 -0400

    Update config

--- backend/azure_services/Config.py
+++ backend/azure_services/Config.py
@@ -1,12 +1,12 @@
 # Computer vision service configuration
 
-VISION_BASE_URL = "https://cv123.cognitiveservices.azure.com/vision/v2.0/"  # Api endpoint
-VISION_KEY = "ecfdde1795b94e56801020b96e5fe33f"  # Subscription Key for computer vision api
+VISION_BASE_URL = "Your Computer Vision Api Endpoint"
+VISION_KEY = "Your Subscription Key for Computer Vision Api"
 
 # Speech Services configuration
-SPEECH_KEY = "ec15e6fdd8ec4948892948406bf1cba1"  # Subscription Key for Speech Service
-SPEECH_FETCH_TOKEN_URL = "https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken"  # Endpoint to get access token for authentication
-SPEECH_SERVICE_RESOURCE_NAME = "ts123"  # Your Speech Service resource name
-SPEECH_SERVICE_URL = "https://eastus.tts.speech.microsoft.com/cognitiveservices/v1"  # The Endpoint of Speech Service
-SPEECH_SERVICE_VOICE_CHOICE = "Microsoft Server Speech Text to Speech Voice (en-CA, Linda)" # The voice choice
+SPEECH_KEY = "Subscription Key for Speech Service"
+SPEECH_FETCH_TOKEN_URL = "Endpoint to get access token for authentication"
+SPEECH_SERVICE_RESOURCE_NAME = "Your Speech Service resource name"
+SPEECH_SERVICE_URL = "The Endpoint of Speech Service"
+SPEECH_SERVICE_VOICE_CHOICE = "The voice choice"
 
